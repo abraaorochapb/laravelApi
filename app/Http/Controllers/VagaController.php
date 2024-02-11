1115a8fc1d868d8db6be1932a043d4d1b1cff59a
abraaorochapb <abraaorochapb@gmail.com>
2024-02-11 15:13:39 -0300

    Adiciona o Controller para o Model Vaga

--- app/Http/Controllers/VagaController.php
+++ app/Http/Controllers/VagaController.php
@@ -3,41 +3,67 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use App\Models\Vaga;
 
 class VagaController extends Controller
 {
     public function index()
     {
-
-    }
-
-    public function create()
-    {
-
+        try {
+        return Vaga::OrderBy('created_at', 'DESC')->get();
+        } catch (\Exception $e) {
+            Log::error('Erro ao salvar vaga: ' . $e->getMessage());
+            return response()->json(['error' => 'Erro ao buscar vagas: ' . $e->getMessage()], 500);
+        }
     }
 
     public function store(Request $request)
     {
-
+        try {
+            $vaga = new Vaga();
+            $vaga->fill($request->all());
+            $vaga->save();
+            return response()->json($vaga, 201);
+        } catch (\Exception $e) {
+            Log::error('Erro ao salvar vaga: ' . $e->getMessage());
+            return response()->json(['error' => 'Erro ao salvar vaga: ' . $e->getMessage()], 500);
+        }
     }
 
     public function show(string $id)
     {
-
-    }
-
-    public function edit(string $id)
-    {
-
+        try {
+            $vaga = Vaga::findOrFail($id);
+            return response()->json($vaga, 200);
+        } catch (\Exception $e) {
+            Log::error('Erro ao buscar vaga: ' . $e->getMessage());
+            return response()->json(['error' => 'Erro ao buscar vaga: ' . $e->getMessage()], 500);
+        }
     }
 
     public function update(Request $request, string $id)
     {
-
+        try {
+            $vagatoupdate = Vaga::findOrFail($id);
+            $vagatoupdate->fill($request->all());
+            $vagatoupdate->save();
+            return response()->json($vagatoupdate, 200);
+        } catch (\Exception $e) {
+            Log::error('Erro ao editar vaga: ' . $e->getMessage());
+            return response()->json(['error' => 'Erro ao editar vaga: ' . $e->getMessage()], 500);
+        }
     }
 
     public function destroy(string $id)
     {
-
+        try {
+            $vaga = Vaga::findOrFail($id);
+            $vaga->delete();
+            return response()->json('Vaga deletada com sucesso', 200);
+        } catch (\Exception $e) {
+            Log::error('Erro ao deletar vaga: ' . $e->getMessage());
+            return response()->json(['error' => 'Erro ao deletar vaga: ' . $e->getMessage()], 500);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,10 +8,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/vagas', [VagaController::class, 'index'])->name('vagas.index');
+Route::get('/vagas', [VagaController::class, 'index'])-> name('vagas.index');
 Route::prefix('/vaga')->group(function () {
-    Route::post('/post', [VagaController::class, 'store'])->name('vagas.store');
-    Route::get('/{id}', [VagaController::class, 'show'])->name('vagas.show');
-    Route::put('/{id}', [VagaController::class, 'update'])->name('vagas.update');
-    Route::delete('/{id}', [VagaController::class, 'destroy'])->name('vagas.destroy');
+    Route::post('/post', [VagaController::class, 'store'])-> name('vagas.store');
+    Route::get('/{id}', [VagaController::class, 'show'])-> name('vagas.show');
+    Route::put('/{id}', [VagaController::class, 'update'])-> name('vagas.update');
+    Route::delete('/{id}', [VagaController::class, 'destroy'])-> name('vagas.destroy');
 });
